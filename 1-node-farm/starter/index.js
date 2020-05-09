0abb3759ba87aca76e20117382373dc474b0fac0
Rob2729 <Rob.niv@hotmail.com>
2020-05-09 17:06:44 +0100

    added prettierrc file

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -4,7 +4,6 @@ const url = require('url');
 const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
-
 /////////////////////////////////////////////////////////------FILES-----//////////////////////////////////////////////////////////////////////////////////////////
 
 // Blocking, synchronous method
@@ -32,66 +31,72 @@ const replaceTemplate = require('./modules/replaceTemplate');
 // console.log('Will Read File!');
 /////////////////////////////////////////////////////////------FILES-----//////////////////////////////////////////////////////////////////////////////////////////
 
-
 /////////////////////////////////////////////////////////------SERVER-----//////////////////////////////////////////////////////////////////////////////////////////
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
 
-const slugs = dataObj.map(el => slugify(el.productName, {
-    lower: true
-}));
+const slugs = dataObj.map((el) =>
+  slugify(el.productName, {
+    lower: true,
+  })
+);
 console.log(slugs);
 
 const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
 
-    const {
-        query,
-        pathname
-    } = url.parse(req.url, true);
+  //OVERVIEW page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
 
+    const cardsHTML = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+    res.end(output);
+  }
+  //PRODUCT page
+  else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
 
-    //OVERVIEW page
-    if (pathname === '/' || pathname === '/overview') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
+    res.end(output);
+  }
+  //API page
+  else if (pathname === '/api') {
+    res.end(data);
+  }
+  //NOT FOUND page
+  else {
+    res.writeHead(404, {
+      'Content-Type': 'text/html',
+    });
+    res.end('<h1>This page cannot be found</h1>');
+  }
 
-        const cardsHTML = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
-        res.end(output);
-    }
-    //PRODUCT page
-    else if (pathname === '/product') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        const product = dataObj[query.id];
-        const output = replaceTemplate(tempProduct, product);
-
-        res.end(output);
-    }
-    //API page
-    else if (pathname === '/api') {
-        res.end(data);
-    }
-    //NOT FOUND page
-    else {
-        res.writeHead(404, {
-            'Content-Type': 'text/html'
-        });
-        res.end('<h1>This page cannot be found</h1>');
-    }
-
-    // res.end('Hello from the server');
+  // res.end('Hello from the server');
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('The servier is listening on port 8000');
+  console.log('The servier is listening on port 8000');
 });
-
 
 /////////////////////////////////////////////////////////------SERVER-----//////////////////////////////////////////////////////////////////////////////////////////
