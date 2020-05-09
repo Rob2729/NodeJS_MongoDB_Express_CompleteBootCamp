a00c581ac9cd528ebafb0ecbcbe7d677114470af
Rob2729 <Rob.niv@hotmail.com>
2020-05-09 15:10:09 +0100

    amended file names to have template in the name, created new empty file for template-card.html

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+
+/////////////////////////////////////////////////////////------FILES-----//////////////////////////////////////////////////////////////////////////////////////////
 
 // Blocking, synchronous method
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -9,17 +14,50 @@ const fs = require('fs');
 // console.log('File Created');
 
 // Non-Blocking, synchronous method
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    if (err) return console.log('ERROR! File Not Found!');
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log('ERROR! File Not Found!');
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3);
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-                console.log("Your file have been written 😃");
-            })
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//                 console.log("Your file have been written 😃");
+//             })
+//         });
+//     });
+// });
+// console.log('Will Read File!');
+/////////////////////////////////////////////////////////------FILES-----//////////////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////------SERVER-----//////////////////////////////////////////////////////////////////////////////////////////
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the overview');
+    } else if (pathName === '/product') {
+        res.end('This is the product');
+    } else if (pathName === '/api') {
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
         });
-    });
+        res.end('<h1>This page cannot be found</h1>');
+    }
+
+    // res.end('Hello from the server');
 });
-console.log('Will Read File!');
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('The servier is listening on port 8000');
+});
+
+
+/////////////////////////////////////////////////////////------SERVER-----//////////////////////////////////////////////////////////////////////////////////////////
