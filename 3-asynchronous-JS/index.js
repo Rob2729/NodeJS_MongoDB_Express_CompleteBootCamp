e6d1558a8f88a264910ee0efed508546afa6d3ca
Rob2729 <Rob.niv@hotmail.com>
2020-05-10 21:17:18 +0100

    implemented using async await to implement the promises - big benefit and makes the coding more easier to read

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -2,23 +2,41 @@ const fs = require('fs');
 const superagent = require('superagent');
 
 const readFilePro = (file) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, (err, data) => {
-      if (err) reject('I could not find that file 🗄');
-      resolve(data);
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) reject('I could not find that file 🗄');
+            resolve(data);
+        });
     });
-  });
 };
 
 const writeFilePro = (file, data) => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(file, data, (err) => {
-      if (err) reject('I could not write to the file');
-      resolve('success');
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, (err) => {
+            if (err) reject('I could not write to the file');
+            resolve('success');
+        });
     });
-  });
 };
 
+const getDocPic = async () => {
+    try {
+        const data = await readFilePro(`${__dirname}/dog.txt`);
+        console.log(`Breed: ${data}`);
+
+        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+        console.log(res.body.message);
+
+        await writeFilePro(`${__dirname}/dog-img.txt`, res.body.message);
+        console.log('Random dog image saved to file');
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+getDocPic();
+
+/*
 readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
     console.log(`Breed: ${data}`);
@@ -40,3 +58,4 @@ readFilePro(`${__dirname}/dog.txt`)
   .catch((err) => {
     console.log(err);
   });
+*/
