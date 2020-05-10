cff8e49ae20af4ccf82ef5d6c40dbf11ea0d179d
Rob2729 <Rob.niv@hotmail.com>
2020-05-10 21:08:29 +0100

    building own promises to understand how they work, and how to create a promise with resolve and reject. also appended then onto each of the promises so they are handled correctly

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -1,20 +1,42 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile(`${__dirname}/dog-img.txt`, res.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log('Random dog image saved to file');
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 🗄');
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('I could not write to the file');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePro(`${__dirname}/dog-img.txt`, res.body.message);
+
+    // fs.writeFile(`${__dirname}/dog-img.txt`, res.body.message, (err) => {
+    //   if (err) return console.log(err.message);
+    //   console.log('Random dog image saved to file');
+    // });
+  })
+  .then(() => {
+    console.log('Random dog image saved to file');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
