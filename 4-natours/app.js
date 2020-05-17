f8c1781070b70aa2945e1586fb24e816a7612343
Rob2729 <Rob.niv@hotmail.com>
2020-05-17 12:41:42 +0100

    started creating the port for the api that will create a new tour, addded in the express middleware as this will allow us to get access to the body of the request

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,19 +1,42 @@
+//core modules go first
+const fs = require('fs');
+
+//node imprted modules
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Hello from the server side',
-        app: 'Natours'
-    });
+app.use(express.json());
+// app.get('/', (req, res) => {
+//     res.status(200).json({
+//         message: 'Hello from the server side',
+//         app: 'Natours'
+//     });
+// });
+
+// app.post('/', (req, res) => {
+//     res.status(200).send('You can post to this endpoint');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-    res.status(200).send('You can post to this endpoint');
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+  res.status(200).json({ message: 'Success' });
 });
-
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`The server is running on port ${port}`);
+  console.log(`The server is running on port ${port}`);
 });
