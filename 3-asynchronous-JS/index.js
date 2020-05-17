a5770c9ce88fb5c096a82eec9c2b8f6ce3e827a2
Rob2729 <Rob.niv@hotmail.com>
2020-05-17 08:26:37 +0100

    used async await to handle multile promises simultaneously

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -2,51 +2,88 @@ const fs = require('fs');
 const superagent = require('superagent');
 
 const readFilePro = (file) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(file, (err, data) => {
-            if (err) reject('I could not find that file 🗄');
-            resolve(data);
-        });
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 🗄');
+      resolve(data);
     });
+  });
 };
 
 const writeFilePro = (file, data) => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(file, data, (err) => {
-            if (err) reject('I could not write to the file');
-            resolve('success');
-        });
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('I could not write to the file');
+      resolve('success');
     });
+  });
 };
 
+// AWAITING MULTIPLE PROMISES SIMULTANEOUSLY EXAMPLE
 const getDogPic = async () => {
-    try {
-        const data = await readFilePro(`${__dirname}/dogg.txt`);
-        console.log(`Breed: ${data}`);
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
 
-        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-        console.log(res.body.message);
+    //Creates variables while will hold the promise
+    const res1Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
 
-        await writeFilePro(`${__dirname}/dog-img.txt`, res.body.message);
-        console.log('Random dog image saved to file');
-    } catch (err) {
-        console.log(err);
-        throw (err);
-    }
-    return '2 : READY 🐶';
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    //create a new variable that will wait for the  promises to be returned
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map(el => el.body.message);
+
+    console.log(imgs);
+
+    await writeFilePro(`${__dirname}/dog-img.txt`, imgs.join('\n'));
+    console.log('Random dog image saved to file');
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+  return '2 : READY 🐶';
 };
+
+/* AWAITING A SINGLE PROMISE EXAMPLE
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro(`${__dirname}/dog-img.txt`, res.body.message);
+    console.log('Random dog image saved to file');
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+  return '2 : READY 🐶';
+}; */
 
 //USES ASYNC AWAIT TO CALL THE FUNCTION.
 (async () => {
-    try {
-        console.log("1: Will get Dog Pics");
-        const x = await getDogPic();
-        console.log(x);
-        console.log("3: Done getting dog pics");
-    } catch (err) {
-        console.log("ERROR 💥");
-    }
-})
+  try {
+    console.log('1: Will get Dog Pics');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics');
+  } catch (err) {
+    console.log('ERROR 💥');
+  }
+})();
 
 /* THIS METHOD USES PROMISES TO CATCH THE ERRORS
 console.log("1: Will get Dog Pics");
@@ -57,8 +94,6 @@ getDogPic().then(x => {
     console.log("ERROR 💥");
 });
 */
-
-
 
 /*
 readFilePro(`${__dirname}/dog.txt`)
