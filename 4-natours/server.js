c65e8eb778e61331bcfa46dc18c772323b6df992
Rob2729 <Rob.niv@hotmail.com>
2020-05-21 21:51:16 +0100

    creation of the model folder and the tourModel

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,8 +1,16 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({
   path: './config.env',
 });
+
+mongoose.connect(process.env.DATABASE_LOCAL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => console.log("DB Connection Successful"));
 
 const app = require('./app');
 
