c2cccbf90ea0e77dafb4a2906e4321303a11619e
Rob2729 <Rob.niv@hotmail.com>
2020-05-21 22:01:52 +0100

    started refactoring the structure

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,17 +1,12 @@
-const fs = require('fs');
 const Tour = require('../models/tourModel');
 
 
-exports.checkID = (req, res, next, val) => {
-    console.log(`Tour id is : ${val}`);
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID',
-        });
-    }
-    next();
-};
+// const tours = JSON.parse(
+//     fs.readFileSync(
+//         `${__dirname}/../dev-data/data/tours-simple.json`
+//     )
+// );
+
 
 exports.checkBody = (req, res, next) => {
     if (!req.body.name || !req.body.price) {
@@ -21,60 +16,40 @@ exports.checkBody = (req, res, next) => {
         });
     }
     next();
-}
+};
 
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
         status: 'Success',
         requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours,
-        },
+        // results: tours.length,
+        // data: {
+        //     tours,
+        // },
     });
 };
 
 exports.getTour = (req, res) => {
     console.log(req.params);
-    const id = req.params.id * 1;
-    const tour = tours.find((el) => el.id === id);
+    //const id = req.params.id * 1;
+    // //const tour = tours.find((el) => el.id === id);
 
-    res.status(200).json({
-        status: 'Success',
-        data: {
-            tour,
-        },
-    });
+    // res.status(200).json({
+    //     status: 'Success',
+    //     data: {
+    //         tour,
+    //     },
+    // });
 };
 
 exports.createTour = (req, res) => {
-    console.log('Start of creating a tour');
-
-    const newID = tours[tours.length - 1].id + 1;
-    const newTours = Object.assign({
-            id: newID,
-        },
-        req.body
-    );
-
-    tours.push(newTours);
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        (err) => {
-            if (err)
-                console.log(
-                    `Something went wrong when writing to file`
-                );
-            res.status(201).json({
-                message: 'Success',
-                data: {
-                    tour: newTours,
-                },
-            });
-        }
-    );
+    res.status(201).json({
+        message: 'Success',
+        // data: {
+        //     tour: newTours,
+        // },
+    });
 };
 
 exports.updateTour = (req, res) => {
@@ -87,8 +62,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-
-
     res.status(204).json({
         status: 'Success',
         data: null,
--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 //Parameter middleware 
 //we get access to 4 variables for param middleware
 // req,res next, val (parameter value)
-router.param('id', tourController.checkID);
+//router.param('id', tourController.checkID);
 
 //in the post example below we have chained middleware functions to the post, so we will always doa checkBody 
 //to validate the request.
