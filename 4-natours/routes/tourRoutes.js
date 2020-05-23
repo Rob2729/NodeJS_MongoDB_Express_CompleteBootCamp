3e3e2d8a3e5570bb79cea30fe6612d43b29bb278
Rob2729 <Rob.niv@hotmail.com>
2020-05-23 19:59:35 +0100

    created an aggregate function to display the power on unwinding, and grouping

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 
-
-
 const router = express.Router();
 
-//Parameter middleware 
+//Parameter middleware
 //we get access to 4 variables for param middleware
 // req,res next, val (parameter value)
 //router.param('id', tourController.checkID);
@@ -13,16 +11,16 @@ const router = express.Router();
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
-//in the post example below we have chained middleware functions to the post, so we will always doa checkBody 
+//in the post example below we have chained middleware functions to the post, so we will always doa checkBody
 //to validate the request.
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 
 router
-    .route('/:id')
-    .get(tourController.getTour)
-    .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
-
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
