325d3f220a92f0f0b646b6ea511e2070bb97e374
Rob2729 <Rob.niv@hotmail.com>
2020-05-24 09:16:33 +0100

    added the ndb package so we can use this to debug our application, also added a middleware after the routes to handle if the route cant be found

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -21,13 +21,8 @@ app.use(express.json());
 //we point to the folder and can display the files inside the folder
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the Middleware');
-  next();
-});
 
 app.use((req, res, next) => {
-  console.log('Hello from Middleware Stage 2');
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -51,5 +46,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//WE CAN ADD MIDDLEWARE AFTER THE ROUTERS TO CATCH ANY ROUTE EXCEPTIONS
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  })
+});
 
 module.exports = app;
