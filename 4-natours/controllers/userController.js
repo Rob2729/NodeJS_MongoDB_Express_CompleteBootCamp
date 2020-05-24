fc01ff679355c10802a6349ea78f375e1eda6e73
Rob2729 <Rob.niv@hotmail.com>
2020-05-24 15:33:27 +0100

    started working on the authentication and the user routes, created the new user model, new authController for sign up created

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -7,24 +7,28 @@ exports.getAllUsers = (req, res) => {
 
 exports.getUser = (req, res) => {
     return res.status(500).json({
-        status: 'Error'
+        status: 'Error',
+        message: 'Route Does not yet exist'
     });
 };
 
 exports.createUser = (req, res) => {
     return res.status(500).json({
-        status: 'Error'
+        status: 'Error',
+        message: 'Route Does not yet exist'
     });
 };
 
 exports.updateUser = (req, res) => {
     return res.status(500).json({
-        status: 'Error'
+        status: 'Error',
+        message: 'Route Does not yet exist'
     });
 };
 
 exports.deleteUser = (req, res) => {
     return res.status(500).json({
-        status: 'Error'
+        status: 'Error',
+        message: 'Route Does not yet exist'
     });
 };
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -2,19 +2,35 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLED EXCEPTION - SHUTTING DOWN');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({
   path: './config.env',
 });
 
-mongoose.connect(process.env.DATABASE_LOCAL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}).then(() => console.log("DB Connection Successful"));
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connection Successful'));
 
 const app = require('./app');
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`The server is running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION - SHUTTING DOWN');
+  server.close(() => {
+    process.exit(1);
+  });
 });
